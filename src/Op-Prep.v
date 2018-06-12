9ff88a61562f3c6b243a94d4eb07eea74c55663b
David Wigley <wigleyd@wit.edu>
2018-06-12 15:59:36 -0400

    Resolve Compile issue

--- src/Op-Prep.v
+++ src/Op-Prep.v
@@ -1,6 +1,7 @@
 module OperationPrep(input wire regWrite, input [4:0] reg1, input[4:0] reg2,
- input[4:0] writeRegister, input[31:0] writeData, output reg [4:0] readData1,
- output reg[4:0] readData2, input wire aluSRC, input reg[31:0] pcOffsetOrig,
- output reg pcOffsetFilled[31:0]);
+  input[4:0] writeRegister, input[31:0] writeData, output reg [4:0] readData1,
+  output reg[4:0] readData2, input wire aluSRC, input [31:0] pcOffsetOrig,
+  output reg [31:0] pcOffsetFilled
+);
 
 endmodule 
