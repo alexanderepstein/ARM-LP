5cc9c823ed92c88ffb8af3c061a78c2f24fcb319
David Wigley <wigleyd@wit.edu>
2018-06-12 15:43:32 -0400

    Added PC offset
    -extension output
    -raw input

--- src/Op-Prep.v
+++ src/Op-Prep.v
@@ -1,5 +1,6 @@
 module OperationPrep(input wire regWrite, input [4:0] reg1, input[4:0] reg2,
  input[4:0] writeRegister, input[31:0] writeData, output reg [4:0] readData1,
- output reg[4:0] readData2, input wire aluSRC);
+ output reg[4:0] readData2, input wire aluSRC, input reg[31:0] pcOffsetOrig,
+ output reg pcOffsetFilled[31:0]);
 
 endmodule 
