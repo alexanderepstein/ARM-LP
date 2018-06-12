521b153c60c9d9ba570a7ac7ce0fb04c7217c0ee
Alex Epstein <epsteina@wit.edu>
2018-06-12 14:16:24 -0400

    Added PC and Op-Prep IO

--- src/Op-Prep.v
+++ src/Op-Prep.v
@@ -1,3 +1,4 @@
-module OperationPrep();
+module OperationPrep(input wire regWrite, input [4:0] reg1, input[4:0] reg2, input[4:0] writeRegister, input[31:0] writeData, 
+		      output reg [4:0] read1, output reg[4:0] read2);
 
 endmodule 
--- src/PC.v
+++ src/PC.v
@@ -1,3 +1,3 @@
-module PC();
+module PC(output reg[31:0] readAddress);
 
 endmodule 
