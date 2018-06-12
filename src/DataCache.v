1f21bdacb0254af0c057a913f5013dd0bd6942d7
David Wigley <wigleyd@wit.edu>
2018-06-12 14:16:49 -0400

    Added enables for read/write

--- src/DataCache.v
+++ src/DataCache.v
@@ -1,3 +1,4 @@
-module DataCache(input[31:0] address, input[3:0] writeData, output reg [31:0] readData);
+module DataCache(input wire memWrite, input wire memRead, input[31:0] address,
+ input[3:0] writeData, output reg [31:0] readData);
 
 endmodule 
