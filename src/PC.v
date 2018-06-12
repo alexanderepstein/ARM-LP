dccf66139cbf65490b457bbcbc4cdf65460f2143
David Wigley <wigleyd@wit.edu>
2018-06-12 16:01:27 -0400

    Resolving Compile Issues

--- src/PC.v
+++ src/PC.v
@@ -1,11 +1,13 @@
-module PC(input wire branchFlag, input wire unconditionalBranchFlag, input wire zeroFlag, input reg[31:0] PCOffset, output reg[31:0] readAddress);
+module PC(input wire branchFlag, input wire unconditionalBranchFlag,
+ input wire zeroFlag, input reg[31:0] PCOffsetOrig, output reg[31:0] readAddress,
+ output reg[31:0] PCScaledOffset);
 	
-	wire muxSelect;
+	reg muxSelect;
 
 	always @ * begin
-		PCOffset = PCOffset << 2; // Multiplying the offset by 4 
+		PCScaledOffset = PCOffsetOrig << 2; // Multiplying the offset by 4 
 		muxSelect = (zeroFlag & branchFlag) | unconditionalBranchFlag; // The mux for what the PC should be set to (branch or next instruction) 
-		readAddress = (muxSelect == 0) ? readAddress + 4 : readAddress + PCOffset;  // If mux == 0 next instruction otherwise branch
+		readAddress = (muxSelect == 0) ? readAddress + 4 : readAddress + PCScaledOffset;  // If mux == 0 next instruction otherwise branch
 	end
 	// Check that you can use the readAddress as an input and output (if i had to guess this is not a valid practice)
 endmodule 
