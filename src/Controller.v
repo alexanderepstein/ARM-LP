565abdc2f6ff99c3b78d19332aa970ba97ea753b
David Wigley <wigleyd@wit.edu>
2018-06-12 14:16:33 -0400

    Added output for the reg2Loc mux select

--- src/Controller.v
+++ src/Controller.v
@@ -1,7 +1,8 @@
-module Controller(input[31:0] address, output wire reg2Loc,
+module Controller(input[31:0] address, output wire reg2LocOut,
     output wire unconditionalBranch, output wire branch, output wire memRead,
     output wire memToReg, output wire aluOP, output wire memWrite,
     output wire aluSRC, output wire regWrite, output reg [4:0] readRegister1,
-    output reg [4:0] readRegister2, output reg [4:0] writeRegister) ;
+    output reg [4:0] readRegister2, output reg [4:0] writeRegister,
+    input wire reg2LocIn) ;
     
 endmodule 
