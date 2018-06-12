2dc4c2136110ef634eb2ff0664b92ebefc7ae6f0
David Wigley <wigleyd@wit.edu>
2018-06-12 15:55:52 -0400

    Fixed Controller to allow Compilation

--- src/Controller.v
+++ src/Controller.v
@@ -1,7 +1,7 @@
-module Controller(input[31:0] address, output wire reg2LocOut,
-    output wire unconditionalBranch, output wire branch, output wire memRead,
-    output wire memToReg, output wire aluOP, output wire memWrite,
-    output wire aluSRC, output wire regWrite, output reg [4:0] readRegister1,
+module Controller(input[31:0] address, output reg reg2LocOut,
+    output reg unconditionalBranch, output reg branch, output reg memRead,
+    output reg memToReg, output reg aluOP, output reg memWrite,
+    output reg aluSRC, output reg regWrite, output reg [4:0] readRegister1,
     output reg [4:0] readRegister2, output reg [4:0] writeRegister,
     input wire reg2LocIn) ;
     
