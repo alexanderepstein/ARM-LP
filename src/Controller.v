27659d5e9957f2e6b211d09394b0a707d265ad1c
David Wigley <wigleyd@wit.edu>
2018-06-12 20:46:30 -0400

    Changed Reg2LocIn
    Reg2LocIn is staying within this module for both input and output and
    can thus become a local var, not parameters.

--- src/Controller.v
+++ src/Controller.v
@@ -1,8 +1,10 @@
-module Controller(input[31:0] address, output reg reg2LocOut,
-    output reg unconditionalBranch, output reg branch, output reg memRead,
-    output reg memToReg, output reg aluOP, output reg memWrite,
-    output reg aluSRC, output reg regWrite, output reg [4:0] readRegister1,
-    output reg [4:0] readRegister2, output reg [4:0] writeRegister,
-    input wire reg2LocIn) ;
+module Controller(input[31:0] instruction, output reg unconditionalBranch,
+    output reg branch, output reg memRead, output reg memToReg,
+    output reg aluOP, output reg memWrite, output reg aluSRC,
+    output reg regWrite, output reg [4:0] readRegister1,
+    output reg [4:0] readRegister2, output reg [4:0] writeRegister) ;
+    
+    //reg2Loc field stays within this field and can thus be handled as a local variable.
+    reg reg2Loc;
     
 endmodule 
