24032d55adf78ad36d157981bfac08660ac54957
Alex Epstein <epsteina@wit.edu>
2018-06-13 16:06:09 -0400

    Changing input and output data types

--- src/Controller.v
+++ src/Controller.v
@@ -1,10 +1,10 @@
-module Controller(input[31:0] instruction, output reg unconditionalBranch,
-    output reg branch, output reg memRead, output reg memToReg,
-    output reg aluOP, output reg memWrite, output reg aluSRC,
-    output reg regWrite, output reg [4:0] readRegister1,
+module Controller(input reg[10:0] opcodeInstruction, output wire unconditionalBranch,
+    output wire branch, output wire memRead, output wire memToReg,
+    output wire aluOP, output wire memWrite, output wire aluSRC,
+    output wire regWrite, output reg [4:0] readRegister1,
     output reg [4:0] readRegister2, output reg [4:0] writeRegister) ;
     
     //reg2Loc field stays within this field and can thus be handled as a local variable.
     reg reg2Loc;
-    
+     
 endmodule 
--- src/DataCache.v
+++ src/DataCache.v
@@ -1,4 +1,4 @@
 module DataCache(input wire memWrite, input wire memRead, input wire memToReg,
- input[31:0] address, input[31:0] writeData, output reg [31:0] readData, input clock);
+ input[31:0] address, input[31:0] writeData, output reg [31:0] readData, input reg clock);
 
 endmodule 
