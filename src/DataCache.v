6cc42bf05043466f594499119e90afb056284f0e
Alex Epstein <epsteina@wit.edu>
2018-06-13 20:38:40 -0400

    Commenting DataCache

--- src/DataCache.v
+++ src/DataCache.v
@@ -1,7 +1,15 @@
-module DataCache(input wire memWrite, input wire memRead, input wire memToReg,
- input[31:0] address, input[31:0] writeData, output reg [31:0] readData, input reg clock);
+module DataCache( memWrite,  memRead, memToReg, address,  writeData,  readData, clock);
+
+ input clock; // Clock
+ input memWrite; // Controls weather we are writing to memory
+ input memRead; // Controls weather we are reading from the memory
+ input memToReg; // Controls weather we are ouputting to a register
+ input [31:0] address; // Address to read or write to
+ input [31:0] writeData; // Data to write into address
+
+ output reg[31:0] readData; // Data read from address
+
 	always @(posedge clock) begin
 
-    end
-endmodule 
-
+  end
+endmodule
