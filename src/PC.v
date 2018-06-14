bae9e03cd3783f82b0dc5171f5a8e245ac4a0ab4
Alex Epstein <epsteina@wit.edu>
2018-06-13 20:35:16 -0400

    Modifying how PC uses muxSelect

--- src/PC.v
+++ src/PC.v
@@ -1,13 +1,20 @@
-module PC(input wire branchFlag, input wire unconditionalBranchFlag,
- input wire zeroFlag, input reg[31:0] PCOffsetOrig, output reg[31:0] readAddress,
- output reg[31:0] PCScaledOffset, input clock);
-	
-	reg muxSelect;
+module PC(branchFlag, unconditionalBranchFlag, zeroFlag, PCOffsetOrig, readAddress, PCScaledOffset, clock);
+
+input clock;
+input branchFlag;
+input unconditionalBranchFlag;
+input zeroFlag;
+input [31:0] PCOffsetOrig;
+
+output reg[31:0] readAddress;
+output reg[31:0] PCScaledOffset;
+
+wire muxSelect = (zeroFlag & branchFlag) | unconditionalBranchFlag; // The mux for what the PC should be set to (branch or next instruction)
+
 
 	always @(posedge clock) begin
-		PCScaledOffset = PCOffsetOrig << 2; // Multiplying the offset by 4 
-		muxSelect = (zeroFlag & branchFlag) | unconditionalBranchFlag; // The mux for what the PC should be set to (branch or next instruction) 
-		readAddress = (muxSelect == 0) ? readAddress + 4 : readAddress + PCScaledOffset;  // If mux == 0 next instruction otherwise branch
+		PCScaledOffset = PCOffsetOrig << 2; // Multiplying the offset by 4
+		readAddress = (muxSelect == 0) ? readAddress + 4 : readAddress + PCScaledOffset; // If mux == 0 next instruction otherwise branch;
 	end
 	// Check that you can use the readAddress as an input and output (if i had to guess this is not a valid practice)
-endmodule 
+endmodule
