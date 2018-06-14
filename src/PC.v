a2063d5381e4b502aadaf031ce15f3669fdaed01
David Wigley <wigleyd@wit.edu>
2018-06-14 18:01:33 -0400

    Resolving compile Issues in PC

--- src/PC.v
+++ src/PC.v
@@ -10,10 +10,12 @@ input [31:0] pcOffsetFilled;
 inout [31:0] PC;
 
 reg [31:0] pcScaledOffset;
-assign PC = (muxSelect == 0) ? PC + 4 : PC + PCScaledOffset; // If mux == 0 next instruction otherwise branch;
 
 wire muxSelect;
 assign muxSelect = (zeroFlag & branchFlag) | unconditionalBranchFlag; // The mux for what the PC should be set to (branch or next instruction)
+
+assign PC = (muxSelect == 0) ? PC + 4 : PC + pcScaledOffset; // If mux == 0 next instruction otherwise branch;
+
 
 	always @(posedge clock) begin
 		pcScaledOffset = pcOffsetFilled << 2; // Multiplying the offset by 4
