c8e03f8d52ecabee57ad09c54556c621e15f5ab4
Alex Epstein <epsteina@wit.edu>
2018-07-01 13:13:24 -0400

    Don't delete the executable if we just want to see the wave

--- src/compile.sh
+++ src/compile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 debug=false
 wave=false
-if [ -f executable.out ]; then rm executable.out;fi
+
 files=$(ls *.v)
 while getopts "dw" opt; do
   case "$opt" in
@@ -11,6 +11,8 @@ while getopts "dw" opt; do
 done
 
 if $wave; then gtkwave -o wave.vcd && rm wave.vcd.fst && exit 0 || exit 1; fi
+
+if [ -f executable.out ]; then rm executable.out;fi
 
 if $debug; then iverilog -o executable.out -s 'top'  $files -D DEBUG && echo "Compiled debug build." || echo "Failure"
 else iverilog -o executable.out -s 'Processor'  $files && echo "Compiled release build." || echo "Failure"
