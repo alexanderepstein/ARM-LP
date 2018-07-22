4d3a46661c71f80fd3f7ab6d80573d4833d86061
David Wigley <wigleyd@wit.edu>
2018-07-22 18:45:34 -0400

    Append PC
    PC logic was going to stream new PC every clock cycle. Added
    synchronization to wait every 5 clock cycles.

--- src/PC.v
+++ src/PC.v
@@ -14,18 +14,26 @@ module PC(branchFlag, unconditionalBranchFlag, zeroFlag,
 
     reg [31:0] pcScaledOffset;
     reg [31:0] internalPCReg;
+    reg syncPC[2:0] = 0; //want to sync the PC every 5th clock cycle
 
     // The mux for what the PC should be set to (branch or next instruction)
     wire muxSelect;
     assign muxSelect = (zeroFlag & branchFlag) | unconditionalBranchFlag;
-
+    
+    //This is not going to work.Going to be updating the PC a shit ton.
     //assign PC to track internalPCReg, which only updates on rising edges
-    assign PC = internalPCReg; // If mux == 0 next instruction otherwise branch;
+    //assign PC = internalPCReg; // If mux == 0 next instruction otherwise branch;
 
 	always @(posedge clock) begin
 		pcScaledOffset <= pcOffsetFilled << 2; // Multiplying the offset by 4
-    internalPCReg <= (muxSelect == 0) ? PC + 4 : PC + pcScaledOffset;
+        internalPCReg <= (muxSelect == 0) ? PC + 4 : PC + pcScaledOffset;
+        syncPC = syncPC + 1;
+        if (syncPC == 5) begin
+            PC = internalPCReg;
+            syncPC = 0;
+        end
 	end
+    
 	// Check that you can use the readAddress as an input and output (if i had to guess this is not a valid practice)
 endmodule
 
