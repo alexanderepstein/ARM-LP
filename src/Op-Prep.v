e7b0b765aa7225585363aa584b3e908d354c9de2
David Wigley <wigleyd@wit.edu>
2018-07-22 18:58:34 -0400

    Changed the read datas to reg
    These should not be wires as its going to create race conditions when
    the write data wants to update the register.

--- src/Op-Prep.v
+++ src/Op-Prep.v
@@ -16,23 +16,28 @@ input [31:0] writeData; // Data to write to writeRegister
 input [31:0] pcOffsetOrig; // Original PC offset
 
 
-output wire [31:0] readData1; // Data from reg1
-output wire [31:0] readData2; // Data from reg2 or instruction
+output reg [31:0] readData1; // Data from reg1
+output reg [31:0] readData2; // Data from reg2 or instruction
 output reg [31:0] pcOffsetFilled; // PC padded with 0s to be 32 bits;
 
 reg [31:0] register[4:0]; //These are the registers
 
+//This screams timing conflict
+//assign readData1 = register[reg1];
+//assign readData2 = register[reg1];
 
 	always @(posedge clock) begin
-        readData1 = register[reg1];
-        readData2 = register[reg2];
+        //I would rather do it this way as it avoid timing conflicts
+        readData1 <= register[reg1];
+        readData2 <= register[reg2];
+        
         //verify that this does not have timing issues. I am reading the data 
         //above first before any potential modification
         if (regWrite == 1) begin  register[writeRegister] = writeData; end
     
         //Sign extending the PC
         //see if opcode is a branch
-        if(pcOffsetOrig[31:26] == 6'b100101 || pcOffsetOrig == 6'000101) begin
+        if(pcOffsetOrig[31:26] == 6'b100101 || pcOffsetOrig == 6'b000101) begin
             pcOffsetFilled[31:0] <= { {6{pcOffsetOrig[25]}}, pcOffsetOrig[25:0] };
         end
         //conditional branch
