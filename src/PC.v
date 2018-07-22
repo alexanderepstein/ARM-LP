4f13dc745552bae5d009151ee01b552e679eb2cd
David Wigley <wigleyd@wit.edu>
2018-07-22 19:08:56 -0400

    Inout Work arounds
    Cannot input and output simultaneously. This is my work around. Just use
    the PC initially and then as soon as we hit 5 cycles, start using our
    new one. Not sure it will latch properly initially but hopefully this
    works.

--- src/PC.v
+++ src/PC.v
@@ -11,15 +11,22 @@ module PC(branchFlag, unconditionalBranchFlag, zeroFlag,
     input [31:0] pcOffsetFilled;
 
     inout [31:0] PC;
+    reg [31:0] PC_in;
+    reg [31:0] PC_out;
 
     reg [31:0] pcScaledOffset;
     reg [31:0] internalPCReg;
-    reg syncPC[2:0] = 0; //want to sync the PC every 5th clock cycle
+    reg [2:0] syncPC; //want to sync the PC every 5th clock cycle
 
     // The mux for what the PC should be set to (branch or next instruction)
     wire muxSelect;
     assign muxSelect = (zeroFlag & branchFlag) | unconditionalBranchFlag;
     
+    initial begin
+        syncPC = 0;
+        PC_out = 0;
+        PC_in = PC;
+    end
     //This is not going to work.Going to be updating the PC a shit ton.
     //assign PC to track internalPCReg, which only updates on rising edges
     //assign PC = internalPCReg; // If mux == 0 next instruction otherwise branch;
@@ -29,7 +36,7 @@ module PC(branchFlag, unconditionalBranchFlag, zeroFlag,
         internalPCReg <= (muxSelect == 0) ? PC + 4 : PC + pcScaledOffset;
         syncPC = syncPC + 1;
         if (syncPC == 5) begin
-            PC = internalPCReg;
+            PC_out = internalPCReg;
             syncPC = 0;
         end
 	end
